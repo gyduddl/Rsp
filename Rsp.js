7fc0228842b5a8ec2b40e6577b8a9bdbb9d8ef06
gyduddl <gyduddl@naver.com>
2023-05-28 22:26:12 +0900

    Rsp완성

--- Rsp.js
+++ Rsp.js
@@ -2,43 +2,55 @@ let Person= ['가위','바위','보'];
 let Victory = ['이겼음','졌음','비겼음'];
 let Computer = Math.floor(Math.random()*3);
 
+//모달창
 const closeModal=()=>{
     let modal = document.getElementById('modal');
     let computerimg = document.getElementById( "computerResult");
     let aimg = document.getElementById("userResult");
-    aimg.src='img/물음표 이미지.jpg';
-    computerimg.src='img/물음표 이미지.jpg';
+    aimg.src='img/물음표.png';
+    computerimg.src='img/물음표2.png';
     modal.style.display='none';
 }
 
+//가위바위보
 const rockClick = async()=>{
         let aimg = document.getElementById("userResult");
-        aimg.src='img/바위 이미지.jpg';
+        aimg.src='img/바위.png';
         RockSissors('바위');
 }
 
 const sissorsClick = ()=>{
     let aimg = document.getElementById("userResult");
-    aimg.src='img/가위 이미지.jpg';
+    aimg.src='img/가위.png';
     RockSissors('가위');
 }
 
 const paperClick = ()=>{
     let aimg = document.getElementById("userResult");
-    aimg.src='img/보 이미지.jpg';
+    aimg.src='img/보.png';
    RockSissors('보');
 }
+//점수변수
+let personValue=0;
+let computerValue=0;
 
-let personValue=[0];
-let computerValue=[0];
+//리셋버튼
+const reset=()=>{
+    let result_num = document.getElementById('result_num')
+    personValue =0;
+    computerValue=0;
+    result_num.innerHTML=`<h2 id="result_num">${personValue} : ${computerValue}</h2>`;
+}
+
+
 
 const RockSissors = (rsp)=>{
     let computerimg = document.getElementById( "computerResult");
     if(Computer===0){
-        computerimg.src='img/가위 이미지.jpg';
+        computerimg.src='img/가위.png';
     }else if(Computer===1){
-        computerimg.src='img/바위 이미지.jpg';
-    }else computerimg.src='img/보 이미지.jpg';
+        computerimg.src='img/바위.png';
+    }else computerimg.src='img/보.png';
 
     if(rsp==='가위'&&Computer===2||rsp==='바위'&&Computer===0
     || rsp==='보'&&Computer===1){
@@ -56,21 +68,20 @@ const RockSissors = (rsp)=>{
 
 
         if(Victory[n]==='졌음') {
-            computerValue=computerValue[0]+1;
-            window.localStorage.setItem('computer', computerValue);
-        }else{
-            personValue=personValue[0]+1;
+            computerValue=computerValue+1;
+            window.localStorage.setItem('computer', JSON.stringify(computerValue));
+        }else if(Victory[n]==='이겼음'){
+            personValue=personValue+1;
             localStorage.setItem('person',JSON.stringify(personValue));
         }
-        
-        let computerValue2= JSON.parse(window.localStorage.getItem("computerValue"))
-        let personValue2=JSON.parse(window.localStorage.getItem("personValue"))
+        //어떻게 localStorage에서 값을 어떻게 가져오는거야.
+        // let computerValue2= window.localStorage.getItem("computerValue");
+        // let personValue2=JSON.parse(localStorage.getItem("personValue"));
     
-        console.log(window.localStorage.getItem("computerValue"))
         setTimeout(()=>  {
         modal.style.display='block';
         result.innerHTML = `<div id="result">${Victory[n]}</div>`;
-        result_num.innerHTML=`<h2 id="result_num">${personValue2} : ${computerValue2}</h2>`;
+        result_num.innerHTML=`<h2 id="result_num">${personValue} : ${computerValue}</h2>`;
     },700 )
     }
 }
